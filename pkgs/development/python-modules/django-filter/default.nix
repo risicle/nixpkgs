d66f41f9672772d2c120b739ef14cec97bed8dc7
Robert Scott <code@humanleg.org.uk>
2019-06-19 22:11:51 +0100

    TMP

--- pkgs/development/python-modules/django-filter/default.nix
+++ pkgs/development/python-modules/django-filter/default.nix
@@ -2,6 +2,11 @@
 , fetchPypi
 , django
 , buildPythonPackage
+, djangorestframework
+, mock
+, django-crispy-forms
+, python
+, fetchpatch
 }:
 buildPythonPackage rec {
 
@@ -13,10 +18,25 @@ buildPythonPackage rec {
     sha256 = "3dafb7d2810790498895c22a1f31b2375795910680ac9c1432821cbedb1e176d";
   };
 
-  doCheck = false;
+  patches = [
+    (fetchpatch {
+      name = "fix-dst-test.patch";
+      url = https://github.com/carltongibson/django-filter/commit/9c0b06610dbd5b69a006ae121d383ef49cc70fff.patch;
+      sha256 = "0rrbqxjcwwkcldxi1q38cf2wcl9rgk7yyyc6rxwswi45r9sxihgb";
+    })
+  ];
+
   propagatedBuildInputs = [
     django
   ];
+  checkInputs = [
+    djangorestframework
+    mock
+    django-crispy-forms
+  ];
+  checkPhase = ''
+    ${python.interpreter} runtests.py
+  '';
 
   meta = with lib; {
     homepage = "https://github.com/carltongibson/django-filter/tree/master";
