e27ac7b227a95ae37f1da275b970b5b53f0b30df
Robert Scott <code@humanleg.org.uk>
2019-06-25 22:25:50 +0100

    lesspass: enable tests

--- pkgs/tools/security/lesspass/default.nix
+++ pkgs/tools/security/lesspass/default.nix
@@ -1,16 +1,33 @@
-{ stdenv, python3 }:
+{ stdenv, python3, fetchFromGitHub }:
 
 let
-  inherit (python3.pkgs) buildPythonApplication fetchPypi;
+  inherit (python3.pkgs) buildPythonApplication pytest mock pexpect;
 in
 buildPythonApplication rec {
   pname = "lesspass";
   version = "8.0.1";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "034qvn1isv27chyyqqsh125isyv18yiny17xfmlw259rwg3851pq";
+  src = fetchFromGitHub {
+    owner = "lesspass";
+    repo = "lesspass";
+    rev = version;
+    sha256 = "0y4kb2lrqls6f89bgm98gjrikmkmxsr5dhcay9dk6kkc1wkndr18";
   };
+  sourceRoot = "source/cli";
+
+  # some tests are designed to run against code in the source directory - adapt to run against
+  # *installed* code
+  postPatch = ''
+    for f in tests/test_functional.py tests/test_interaction.py ; do
+      substituteInPlace $f --replace "lesspass/core.py" "-m lesspass.core"
+    done
+  '';
+
+  checkInputs = [ pytest mock pexpect ];
+  checkPhase = ''
+    mv lesspass lesspass.hidden  # ensure we're testing against *installed* package
+    pytest tests
+  '';
 
   meta = with stdenv.lib; {
     description = "Stateless password manager";
