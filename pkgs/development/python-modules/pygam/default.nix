66e70a797207646b5bd4cd8f9e2237157627e9db
Brian Lewis <brianburkelewis@gmail.com>
2019-09-06 13:20:09 -0700

    pygam: init at 0.8.0

---
+++ pkgs/development/python-modules/pygam/default.nix
@@ -0,0 +1,29 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, future
+, numpy
+, pandas
+, progressbar2
+, scipy
+, pytest
+}:
+
+buildPythonPackage rec {
+  pname = "pygam";
+  version = "0.8.0";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "0rli8w0fgfr6mxk5cx3n6i5zafi1j0abl2nslrrdxzmjm2p03bjw";
+  };
+
+  propagatedBuildInputs = [ future numpy pandas progressbar2 scipy ];
+  checkInputs = [ pytest ];
+
+  meta = with lib; {
+    description = "A Python implementation of Generalized Additive Models";
+    homepage = https://github.com/dswah/pyGAM;
+    license = licenses.asl20; # unspecified
+  };
+}
