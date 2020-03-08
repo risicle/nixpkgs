e13f9845997bf688a80b819bec14345bb780ef99
Robert Scott <code@humanleg.org.uk>
2020-03-08 16:08:51 +0000

    gperftools: custom tcmalloc

--- pkgs/development/libraries/gperftools/default.nix
+++ pkgs/development/libraries/gperftools/default.nix
@@ -1,19 +1,29 @@
-{ stdenv, fetchurl, libunwind }:
+{ stdenv, fetchFromGitHub, libunwind, autoconf, automake, libtool }:
 
 stdenv.mkDerivation rec {
-  name = "gperftools-2.7";
+  name = "gperftools-2.7ris";
 
-  src = fetchurl {
-    url = "https://github.com/gperftools/gperftools/releases/download/${name}/${name}.tar.gz";
-    sha256 = "1jb30zxmw7h9qxa8yi76rfxj4ssk60rv8n9y41m6pzqfk9lwis0y";
+#   src = fetchurl {
+#     url = "https://github.com/gperftools/gperftools/releases/download/ris-readable-page-fence-0/ris-readable-page-fence-0.tar.gz";
+#     sha256 = "1jb30zxm4444qxa8yi76rfxj4ssk60rv8n9y41m6pzqfk9lwis0y";
+#   };
+  src = fetchFromGitHub {
+    owner = "risicle";
+    repo = "gperftools";
+    rev = "ris-readable-page-fence-0";
+    sha256 = "025bj9w5v6pry0aizrrs0kwyka9w60w28mijfxn1ay698zflydyn";
   };
 
-  buildInputs = stdenv.lib.optional stdenv.isLinux libunwind;
+  buildInputs = [ autoconf automake libtool ] ++ stdenv.lib.optional stdenv.isLinux libunwind;
 
   prePatch = stdenv.lib.optionalString stdenv.isDarwin ''
     substituteInPlace Makefile.am --replace stdc++ c++
     substituteInPlace Makefile.in --replace stdc++ c++
     substituteInPlace libtool --replace stdc++ c++
+  '';
+
+  preConfigure = ''
+    sh autogen.sh
   '';
 
   NIX_CFLAGS_COMPILE = stdenv.lib.optionals stdenv.isDarwin [
