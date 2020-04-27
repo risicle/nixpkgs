9e1f68f4faa5a16eadf8d9c5e0ec2d10c8378106
Robert Scott <code@humanleg.org.uk>
2020-04-27 18:48:21 +0100

    ijson: afl building

--- pkgs/development/python-modules/ijson/default.nix
+++ pkgs/development/python-modules/ijson/default.nix
@@ -1,15 +1,25 @@
-{ stdenv, buildPythonPackage, fetchPypi }:
+{ stdenv, buildPythonPackage, fetchPypi, yajl, cffi, aflplusplus }:
 
 buildPythonPackage rec {
   pname = "ijson";
-  version = "2.5.1";
+  version = "3.0.3";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "19ec46a2f7991004e5202ecee56c569616b8a7f95686ad7fd0a9ec81cac00269";
+    sha256 = "1774vhabygdiq1avkir5nmavcxrr5npx5nnr6anxgh9zpdsf1rm1";
   };
 
+  propagatedBuildInputs = [ yajl cffi ];
+
   doCheck = false; # something about yajl
+
+  AFL_HARDEN="1";
+  AFL_LLVM_LAF_SPLIT_SWITCHES="1";
+  AFL_LLVM_LAF_TRANSFORM_COMPARES="1";
+  AFL_LLVM_LAF_SPLIT_COMPARES="1";
+  preConfigure = ''
+    export CC=${aflplusplus}/bin/afl-clang-fast
+  '';
 
   meta = with stdenv.lib; {
     description = "Iterative JSON parser with a standard Python iterator interface";
