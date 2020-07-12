45355031e30bc0e09d05d77c1f1188fa390ed5ac
Robert Scott <code@humanleg.org.uk>
2020-07-12 16:22:29 +0100

    cassandra-driver: AFL adaptations

--- pkgs/development/python-modules/cassandra-driver/default.nix
+++ pkgs/development/python-modules/cassandra-driver/default.nix
@@ -11,6 +11,7 @@
 , scales
 , six
 , sure
+, aflplusplus
 }:
 
 buildPythonPackage rec {
@@ -22,6 +23,11 @@ buildPythonPackage rec {
     sha256 = "03nycyn5nd1pnrg6fffq3wcjqnw13lgja137zq5zszx68mc15wnl";
   };
 
+  patches = [
+#     ./ris-print-io-buffer.patch
+#     ./ris-print-stream-id.patch
+  ];
+
   nativeBuildInputs = [ cython ];
   buildInputs = [ libev ];
   propagatedBuildInputs = [ six ]
@@ -29,6 +35,7 @@ buildPythonPackage rec {
 
   checkInputs = [ eventlet mock nose pytest pytz pyyaml sure ];
 
+  doCheck = false;
   # ignore test files which try to do socket.getprotocolname('tcp')
   # as it fails in sandbox mode due to lack of a /etc/protocols file
   checkPhase = ''
@@ -36,6 +43,16 @@ buildPythonPackage rec {
       --ignore=tests/unit/io/test_libevreactor.py \
       --ignore=tests/unit/io/test_eventletreactor.py \
       --ignore=tests/unit/io/test_asyncorereactor.py
+  '';
+
+  AFL_HARDEN="1";
+  AFL_LLVM_LAF_SPLIT_SWITCHES="1";
+  AFL_LLVM_LAF_TRANSFORM_COMPARES="1";
+  AFL_LLVM_LAF_SPLIT_COMPARES="1";
+  AFL_LLVM_INSTRIM="1";
+  AFL_LLVM_NOT_ZERO="1";
+  preConfigure = ''
+    export CC=${aflplusplus}/bin/afl-clang-fast
   '';
 
   meta = with lib; {
