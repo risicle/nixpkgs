6a82f4b5ff698f1172a37f38277212c1d5159ed1
Robert Scott <code@humanleg.org.uk>
2020-10-17 18:48:34 +0100

    html5-parser: AFL adaptations

--- pkgs/development/python-modules/html5-parser/default.nix
+++ pkgs/development/python-modules/html5-parser/default.nix
@@ -1,5 +1,19 @@
-{ stdenv, buildPythonPackage, fetchPypi, pkgs, pkgconfig, chardet, lxml }:
+{ stdenv, buildPythonPackage, fetchPypi, pkgs, pkgconfig, chardet, lxml, aflplusplus }:
 
+let
+  lxml' = lxml.overrideAttrs (oldAttrs: {
+#     AFL_HARDEN="1";
+    AFL_LLVM_LAF_SPLIT_SWITCHES="1";
+    AFL_LLVM_LAF_TRANSFORM_COMPARES="1";
+    AFL_LLVM_LAF_SPLIT_COMPARES="1";
+    AFL_LLVM_INSTRIM="1";
+    AFL_LLVM_NOT_ZERO="1";
+    preConfigure = ''
+        export CC=${aflplusplus}/bin/afl-clang-fast
+    '';
+    pythonImportsCheck = [];
+  });
+in
 buildPythonPackage rec {
   pname = "html5-parser";
   version = "0.4.9";
@@ -9,8 +23,12 @@ buildPythonPackage rec {
     sha256 = "25fe8f6848cbc15187f6748c0695df32bcf1b37df6420b6a01b4ebe1ec1ed48f";
   };
 
+  patches = [
+    ./crash-on-forbidden.patch
+  ];
+
   nativeBuildInputs = [ pkgconfig ];
-  propagatedBuildInputs = [ chardet lxml pkgs.libxml2 ];
+  propagatedBuildInputs = [ chardet lxml' pkgs.libxml2 ];
 
   doCheck = false; # No such file or directory: 'run_tests.py'
 
@@ -19,4 +37,14 @@ buildPythonPackage rec {
     homepage = https://html5-parser.readthedocs.io;
     license = licenses.asl20;
   };
+#   dontStrip = true;
+#   AFL_HARDEN="1";
+  AFL_LLVM_LAF_SPLIT_SWITCHES="1";
+  AFL_LLVM_LAF_TRANSFORM_COMPARES="1";
+  AFL_LLVM_LAF_SPLIT_COMPARES="1";
+  AFL_LLVM_INSTRIM="1";
+  AFL_LLVM_NOT_ZERO="1";
+  preConfigure = ''
+    export CC=${aflplusplus}/bin/afl-clang-fast
+  '';
 }
