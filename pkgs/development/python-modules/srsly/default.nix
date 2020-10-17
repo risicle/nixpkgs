53c7244e7952e3dbf9df4a0c6398310981689fa8
Robert Scott <code@humanleg.org.uk>
2020-10-17 18:53:23 +0100

    srsly: AFL adaptations

--- pkgs/development/python-modules/srsly/default.nix
+++ pkgs/development/python-modules/srsly/default.nix
@@ -8,10 +8,18 @@
 , pathlib
 , pytest
 , pytz
+, cython
+, aflplusplus
 }:
 
 buildPythonPackage rec {
   pname = "srsly";
+#   version = "2.2.0";
+# 
+#   src = fetchPypi {
+#     inherit pname version;
+#     sha256 = "1h246zvh2wsqyjlw3a3bwmd1zwrkgpflk4z4i9k3sqp2j1jika54";
+#   };
   version = "0.2.0";
 
   src = fetchPypi {
@@ -19,14 +27,27 @@ buildPythonPackage rec {
     sha256 = "0gha1xfh64mapvgn0sghnjsvmjdrh5rywhs3j3bhkvwk42kf40ma";
   };
 
+  buildInputs = [ cython ];
   propagatedBuildInputs = lib.optional (pythonOlder "3.4") pathlib;
 
-  checkInputs = [
-    mock
-    numpy
-    pytest
-    pytz
-  ];
+#   dontStrip = true;
+#   AFL_HARDEN="1";
+#   AFL_LLVM_LAF_SPLIT_SWITCHES="1";
+#   AFL_LLVM_LAF_TRANSFORM_COMPARES="1";
+#   AFL_LLVM_LAF_SPLIT_COMPARES="1";
+#   AFL_LLVM_INSTRIM="1";
+#   AFL_LLVM_NOT_ZERO="1";
+#   preConfigure = ''
+#     export CC=${aflplusplus}/bin/afl-clang-fast
+#   '';
+#   NIX_CFLAGS_COMPILE="-I${stdenv.cc.cc}/include/c++/${stdenv.cc.cc.version} -I${stdenv.cc.cc}/include/c++/${stdenv.cc.cc.version}/${stdenv.targetPlatform.config}";
+
+#   checkInputs = [
+#     mock
+#     numpy
+#     pytest
+#     pytz
+#   ];
 
   # TypeError: cannot serialize '_io.BufferedRandom' object
   # Possibly because of sandbox restrictions.
