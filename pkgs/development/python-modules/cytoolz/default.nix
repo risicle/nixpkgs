b2dcea70792363fd2aaa485dc2deba1b9931ceb8
Robert Scott <code@humanleg.org.uk>
2020-10-17 18:55:11 +0100

    cytoolz: AFL adaptations

--- pkgs/development/python-modules/cytoolz/default.nix
+++ pkgs/development/python-modules/cytoolz/default.nix
@@ -6,6 +6,7 @@
 , toolz
 , python
 , fetchpatch
+, aflplusplus
 }:
 
 buildPythonPackage rec {
@@ -16,6 +17,17 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "0p4a9nadsy1337gy2cnb5yanbn03j3zm6d9adyqad9bk3nlbpxc2";
   };
+
+  AFL_HARDEN="1";
+  AFL_LLVM_LAF_SPLIT_SWITCHES="1";
+  AFL_LLVM_LAF_TRANSFORM_COMPARES="1";
+  AFL_LLVM_LAF_SPLIT_COMPARES="1";
+  AFL_LLVM_INSTRIM="1";
+  AFL_LLVM_NOT_ZERO="1";
+  preConfigure = ''
+    export CC=${aflplusplus}/bin/afl-clang-fast
+  '';
+  doCheck = false;
 
   # Extension types
   disabled = isPyPy;
