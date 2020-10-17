9ab7a4ef2f07624cda5ca3e8fcf5c8b7f968007f
Robert Scott <code@humanleg.org.uk>
2020-10-17 18:47:20 +0100

    cbor2: AFL adaptations

--- pkgs/development/python-modules/cbor2/default.nix
+++ pkgs/development/python-modules/cbor2/default.nix
@@ -1,18 +1,32 @@
-{ lib, buildPythonPackage, fetchPypi, pytest, pytestcov, setuptools_scm }:
+{ lib, buildPythonPackage, fetchPypi, pytest, pytestcov, setuptools_scm, aflplusplus }:
 
 buildPythonPackage rec {
   pname = "cbor2";
-  version = "4.1.2";
+  version = "5.1.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1bp9l3wdj0wm15xlmlcwbgv6hc6vcfx39nssikj8fkwnd7d1bdhp";
+    sha256 = "1rhxsbn39xsrrkqzzwlcrap0nwvh2razlk8l4kxd9w2yxv89nxci";
   };
 
   nativeBuildInputs = [ setuptools_scm ];
   checkInputs = [ pytest pytestcov ];
 
+  doCheck = false;
   checkPhase = "pytest";
+
+  dontStrip = true;
+  NIX_CFLAGS_COMPILE=["-O1"];
+
+#   AFL_HARDEN="1";
+#   AFL_LLVM_LAF_SPLIT_SWITCHES="1";
+#   AFL_LLVM_LAF_TRANSFORM_COMPARES="1";
+#   AFL_LLVM_LAF_SPLIT_COMPARES="1";
+#   AFL_LLVM_INSTRIM="1";
+#   AFL_LLVM_NOT_ZERO="1";
+#   preConfigure = ''
+#     export CC=${aflplusplus}/bin/afl-clang-fast
+#   '';
 
   meta = with lib; {
     description = "Pure Python CBOR (de)serializer with extensive tag support";
