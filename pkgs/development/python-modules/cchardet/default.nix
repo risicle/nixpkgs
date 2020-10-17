5304e12ba3fd0353882d56946911b8c4659a981c
Robert Scott <code@humanleg.org.uk>
2020-10-17 18:49:47 +0100

    cchardet: AFL adaptations

--- pkgs/development/python-modules/cchardet/default.nix
+++ pkgs/development/python-modules/cchardet/default.nix
@@ -1,23 +1,37 @@
-{ lib
+{ stdenv
+, lib
 , buildPythonPackage
 , fetchPypi
 , python
 , nose
+, aflplusplus
 }:
 
 buildPythonPackage rec {
   pname = "cchardet";
-  version = "2.1.5";
+  version = "2.1.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "240efe3f255f916769458343840b9c6403cf3192720bc5129792cbcb88bf72fb";
+    sha256 = "1cs6y59qhbal8fgbyjk2lpjykh8kfjhq16clfssylsddb4hgnsmp";
   };
 
-  checkInputs = [ nose ];
-  checkPhase = ''
-    ${python.interpreter} setup.py nosetests
+#   checkInputs = [ nose ];
+#   checkPhase = ''
+#     ${python.interpreter} setup.py nosetests
+#   '';
+
+#   dontStrip = true;
+  AFL_HARDEN="1";
+  AFL_LLVM_LAF_SPLIT_SWITCHES="1";
+  AFL_LLVM_LAF_TRANSFORM_COMPARES="1";
+  AFL_LLVM_LAF_SPLIT_COMPARES="1";
+  AFL_LLVM_INSTRIM="1";
+  AFL_LLVM_NOT_ZERO="1";
+  preConfigure = ''
+    export CC=${aflplusplus}/bin/afl-clang-fast
   '';
+  NIX_CFLAGS_COMPILE="-I${stdenv.cc.cc}/include/c++/${stdenv.cc.cc.version} -I${stdenv.cc.cc}/include/c++/${stdenv.cc.cc.version}/${stdenv.targetPlatform.config}";
 
   meta = {
     description = "High-speed universal character encoding detector";
