c6cd51f5b84718ea965402aa814961c4f9ecd344
Robert Scott <code@humanleg.org.uk>
2021-08-21 16:50:36 +0100

    radare2: 5.3.1 -> 5.4.0

--- pkgs/development/tools/analysis/radare2/default.nix
+++ pkgs/development/tools/analysis/radare2/default.nix
@@ -28,14 +28,32 @@
 
 stdenv.mkDerivation rec {
   pname = "radare2";
-  version = "5.3.1";
+  version = "5.4.0";
 
   src = fetchFromGitHub {
     owner = "radare";
     repo = "radare2";
     rev = version;
-    sha256 = "sha256-VS8eG5RXwKtJSLmyaSifopJU7WYGMUcznn+burPqEYE=";
+    sha256 = "170q44jjac96jhh6fs723nwyr8vig058larpg9yf3l2n94kwq499";
   };
+
+  # replace build system's attempt to clone vector35 aarch64 disassembler
+  # from github
+  vector35ArchArm64Src = fetchFromGitHub {
+    owner = "radareorg";
+    repo = "vector35-arch-arm64";
+    # no tags in repo. build system attempts to fetch latest in branch
+    # named `radare2`, which is this revision at time of writing.
+    rev = "5837915960c2ce862a77c99a374abfb7d18a8534";
+    sha256 = "0vpfqvr2r0vbd7072iv1jp16j7ybgf320aksyxsi1dzywn631kvf";
+  };
+  postPatch = ''
+    cp -r $vector35ArchArm64Src libr/asm/arch/arm/v35arm64/arch-arm64
+    chmod -R +w libr/asm/arch/arm/v35arm64/arch-arm64
+    substituteInPlace libr/asm/arch/arm/v35arm64/Makefile \
+      --replace 'git clone' '#git clone' \
+      --replace 'cd arch-arm64 && git' '#cd arch-arm64 && git'
+  '';
 
   postInstall = ''
     install -D -m755 $src/binr/r2pm/r2pm $out/bin/r2pm
@@ -83,7 +101,8 @@ stdenv.mkDerivation rec {
   meta = {
     description = "unix-like reverse engineering framework and commandline tools";
     homepage = "http://radare.org/";
-    license = lib.licenses.gpl2Plus;
+    # vector35 aarch64 disassembler is asl20
+    license = with lib.licenses [ gpl2Plus asl20 ];
     maintainers = with lib.maintainers; [ raskin makefu mic92 ];
     platforms = with lib.platforms; linux;
   };
