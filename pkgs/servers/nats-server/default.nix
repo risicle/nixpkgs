e9158ea0b511024dc16187bdecb517ebb8855610
Robert Scott <code@humanleg.org.uk>
2022-03-13 14:42:51 +0000

    TMP

--- pkgs/servers/nats-server/default.nix
+++ pkgs/servers/nats-server/default.nix
@@ -1,4 +1,4 @@
-{  buildGoPackage, fetchFromGitHub, lib, fetchpatch }:
+{  buildGoPackage, fetchFromGitHub, lib, fetchpatch, fetchurl }:
 
 with lib;
 
@@ -19,6 +19,15 @@ buildGoPackage rec {
     #./2.6.0-CVE-2022-24450.patch
     ./2.6.0-CVE-2022-26652.patch
   ];
+
+  postPatch = let
+    stream-tar-s2 = fetchurl {
+      url = "https://github.com/nats-io/nats-server/raw/b4128693ed61aa0c32179af07677bcf1d8301dcd/test/configs/jetstream/restore_bad_stream/stream.tar.s2";
+      sha256 = "04y5vhazhyk354g0b2ymqfw42fmaalzqy6zacnd7c5z6a5v243jn";
+    };
+  in ''
+    cp ${stream-tar-s2} test/configs/jetstream/restore_bad_stream/stream.tar.s2
+  '';
 
   doCheck = true;
   checkPhase = ''
