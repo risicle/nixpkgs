45aa70427265239e8a6d9b293d074c92dd0b625e
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-03-31 01:05:12 +0200

    python3Packages.numpy: 1.21.5 -> 1.22.3

--- pkgs/development/python-modules/numpy/default.nix
+++ pkgs/development/python-modules/numpy/default.nix
@@ -44,7 +44,7 @@ in buildPythonPackage rec {
   # Attention! v1.22.0 breaks scipy and by extension scikit-learn, so
   # build both to verify they don't break.
   # https://github.com/scipy/scipy/issues/15414
-  version = "1.21.5";
+  version = "1.22.3";
 
   format = "pyproject.toml";
   disabled = pythonOlder "3.7";
@@ -52,7 +52,7 @@ in buildPythonPackage rec {
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "sha256-alkovGJBJk3OXtUJ5m8zZ2/Jf0ZOepGe3GcvtVMiIe4=";
+    sha256 = "sha256-28dgGjt0ctVZ3HuTOxi0tm+ap0UsEg6H37M9AgCMihg=";
   };
 
   patches = lib.optionals python.hasDistutilsCxxPatch [
